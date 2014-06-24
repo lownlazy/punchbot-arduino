e7d2fe987141608f38ae3dff0a106333efa5af8f
lownlazy <lownlazy@gmail.com>
2014-06-24 22:05:07 +0800

    add copper strip start switch - improved start response time add auto reset when contact with start switch

--- collector/collector.ino
+++ collector/collector.ino
@@ -8,10 +8,11 @@ int triggerPin = 3;
 
 //encoder vars
 volatile int segCounter = 0; 
-volatile int segCountMax = 150; 
-volatile unsigned long segArray[150];
-volatile int segPos[150];
+volatile int segCountMax = 100; 
+volatile unsigned long segArray[100];
+volatile int segPos[100];
 volatile int pos = 0;
+unsigned long start = 550;
 
 //vars
 volatile int isOpen = 0;
@@ -22,7 +23,7 @@ void setup() {
  pinMode(triggerPin, INPUT);
   pinMode(resetBtnPin, INPUT);
   attachInterrupt(0,  encoderInterupt, CHANGE);
- //attachInterrupt(1,  encoderInterupt, CHANGE);
+ attachInterrupt(1,  encoderInterupt, CHANGE);
 
  Serial.begin(9600);
 }
@@ -30,7 +31,20 @@ void setup() {
 void loop() 
 {
   setIsOpen();
+  
+  if(isOpen == 0 )
+  {
+    start = micros();
     
+    if(segCounter == 999)
+    {
+       Serial.println("reset");
+    }
+    
+    pos=0;
+    segCounter=0;    
+  }
+  
   int resetBtnVal = analogRead(resetBtnPin);
       
   if(resetBtnVal > 700){
@@ -39,10 +53,11 @@ void loop()
 
   if( segCounter == segCountMax) 
   {
+      Serial.println(0);
       int i = 1;  
-      for (i = 1; i < segCountMax; i++) 
+      for (i = 0; i < segCountMax; i++) 
       { 
-        unsigned long first = segArray[i-1] - segArray[0];      
+        unsigned long first = segArray[i] - start;      
         Serial.println(first); 
       }
       
@@ -52,7 +67,7 @@ void loop()
       isOpen = 0;  
   }
 
-  delay(500);
+  //delay(50);
 } 
 
 void reset()
@@ -74,14 +89,7 @@ void encoderInterupt()
 
 void setIsOpen()
 {
-  int openVal = analogRead(startContactPin);
-  openState = openVal<100?1:0;
-
-  if (openState != lastOpenState) 
-  {
-    isOpen = openState; 
-  }
-  lastOpenState = openState;
- }
-
+  float openVal = analogRead(startContactPin);
+  isOpen = openVal<200?1:0;
+}
 
